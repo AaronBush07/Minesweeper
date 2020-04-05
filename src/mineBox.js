33b2cb1aa1efd65d8120676dc926e652c85c6b58
Aaronbush07 <aaronbush07@bitbucket.org>
2020-04-06 01:57:53 +1000

    Clicking mapped

---
+++ src/mineBox.js
@@ -0,0 +1,51 @@
+class mineBox {
+	/*
+		state: 
+			blank: 0;
+			mined: 1;
+			number: 2;
+	*/
+	constructor(x, y)
+	{
+		//initial state of the box. 
+		this._x = x;
+		this._y = y;
+		this._state = 0;
+		this._mine = false;
+		this._isOpen = false;
+		this._minesAdj = 0;
+		this._leftMouse = false;
+
+	}
+
+	display()
+	{
+		//console.log(this._x, this._y);
+		if (this._leftMouse == false)
+		{
+			color("white");
+			square(this._x, this._y, sqSize);
+		}
+		else
+		{
+			color("green");
+			square(this._x, this._y, sqSize);
+		}
+		textAlign(LEFT, TOP);
+		text(this._x + "," + this._y, this._x, this._y);
+	}
+
+	clicked() {
+		console.log("Hey I was clicked");
+	}
+
+	set mine(value) {
+		this._mine = true;
+	}
+
+	get mine() {
+		return this._mine;
+	}
+
+
+}
--- src/sketch.js
+++ src/sketch.js
@@ -1,37 +1,29 @@
 const canvasX = 500;
 const canvasY = 800;
 const mines = 10;
-const sqWidth = 5;
-const sqLength = 8;
+const sqX = 5;
+const sqY = 8;
 const sqSize = 100;
 let cnv;
-var mineBoxArray = new Array(sqWidth);
+var mineBoxArray = new Array(sqX);
 
 function createGame() {
 	let counter = 0;
-	for (let i = 0; i < sqWidth; i++) 
+	for (let i = 0; i < sqX; i++) 
 	{
-		mineBoxArray[i] = new Array(sqLength);
+		mineBoxArray[i] = new Array(sqY);
 		console.log(mineBoxArray[i]);
 	}
 
-	for (let i = 0; i < sqWidth; i++)
+	for (let i = 0; i < sqX; i++)
 	{
 
-		for (let k = 0; k < sqLength; k++)
+		for (let k = 0; k < sqY; k++)
 		{
 			mineBoxArray[i][k] = new mineBox(i*sqSize, k*sqSize);
 			counter++;
 		}
 	}
-	// for(let i = 0; i < canvasX; i = i + sqSize)
- //  	{
-	// 	for(let k = 0; k < canvasY; k = k + sqSize)
-	// 	{
-	// 		/*Create new mineboxes.*/
-	//     	mineBoxArray.push(new mineBox(i, k));
-	// 	}
- //  }
    return counter;
 }
 
@@ -45,67 +37,33 @@ function setup() {
 
 /*Mouse pressed somewhere on canvas. Calculate the location based on mouse*/
 function mousePressed() {
-  
+  let x = Math.floor(mouseX / sqSize);
+  let y = Math.floor(mouseY / sqSize);
+  console.log("Mouse pressed at:", x, y); 
+  if ((x >= 0 && x < sqX) && (y >= 0 && y < sqY))
+  {
+  	mineBoxArray[x][y].clicked();
+  } 
+}
+
+function mouseReleased() {
+  let x = Math.floor(mouseX / sqSize);
+  let y = Math.floor(mouseY / sqSize);
+  console.log("Mouse released at:", x, y);
 }
 
 
 function draw() {
   //console.log(sqSize);
   background(153);
-  for (let i = 0; i < sqWidth; i++)
+  for (let i = 0; i < sqX; i++)
   {
-  	for (let k = 0; k < sqLength; k++)
+  	for (let k = 0; k < sqY; k++)
 	{
-		console.log(i,k);
+		//console.log(i,k);
 		mineBoxArray[i][k].display();
 	}
   }
   
 }
 
-class mineBox {
-	/*
-		state: 
-			blank: 0;
-			mined: 1;
-			number: 2;
-	*/
-	constructor(x, y)
-	{
-		//initial state of the box. 
-		this._x = x;
-		this._y = y;
-		this._state = 0;
-		this._isOpen = false;
-		this._minesAdj = 0;
-		this._leftMouse = false;
-
-	}
-
-	display()
-	{
-		//console.log(this._x, this._y);
-		if (this._leftMouse == false)
-		{
-			color("white");
-			square(this._x, this._y, sqSize);
-		}
-		else
-		{
-			color("green");
-			square(this._x, this._y, sqSize);
-		}
-		textAlign(LEFT, TOP);
-		text(this._x + "," + this._y, this._x, this._y);
-	}
-
-	mousePressed()
-	{
-		console.log('Mouse pressed on ${this._x}, ${this._y}');
-		this._leftMouse = true;
-	}
-	mouseReleased()
-	{
-		this._leftMouse = false;
-	}
-}
