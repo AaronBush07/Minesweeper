e9ee2d556a965d6c1e95acb08fac144d0a6dc785
Aaronbush07 <aaronbush07@bitbucket.org>
2020-04-05 22:10:32 +1000

    Square design

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,29 @@
+let canvasLength = 500;
+let canvasWidth = 800;
+let mines = 10;
+let sqWidth = 5;
+let sqLength = 8;
+let sqSize = 100;
+
 function setup() {
   // put setup code here
+  createCanvas(canvasWidth, canvasLength);
+  background(153);
 }
 
 function draw() {
   // put drawing code here
-  ellipse(50,50,80,80);
+  //console.log(canvasLength);
+  //console.log(canvasWidth);
+
+  console.log(sqSize);
+  for(let i = 0; i < canvasWidth; i = i + sqSize)
+  {
+	for(let k = 0; k < canvasLength; k = k + sqSize)
+	{
+
+    	square(i, k, sqSize);
+ 	//console.log(i, k, sqSize);
+	}
+  }
 }
